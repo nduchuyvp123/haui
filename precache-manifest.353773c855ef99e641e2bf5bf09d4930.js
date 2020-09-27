e2ef3179998d7832429e76b77e8109c9d4c53488
Nguyen Duc Huy <61613859+nduchuyvp123@users.noreply.github.com>
2020-09-27 21:50:23 +0700

    Add files via upload

---
+++ precache-manifest.353773c855ef99e641e2bf5bf09d4930.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "841a9527cda66c1133904800a67e9ffa",
+    "url": "/haui-app/index.html"
+  },
+  {
+    "revision": "189bca4bdf134374e633",
+    "url": "/haui-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "72a52e286cfb29df1bdb",
+    "url": "/haui-app/static/css/main.8e2baab7.chunk.css"
+  },
+  {
+    "revision": "189bca4bdf134374e633",
+    "url": "/haui-app/static/js/2.8230b3d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/haui-app/static/js/2.8230b3d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a52e286cfb29df1bdb",
+    "url": "/haui-app/static/js/main.02d34f74.chunk.js"
+  },
+  {
+    "revision": "688eae0032b6a0eeaf7e",
+    "url": "/haui-app/static/js/runtime-main.434ee799.js"
+  }
+]);
